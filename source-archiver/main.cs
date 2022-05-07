730e7b3313e0c4e395469ecf22bb0af41361f350
Gabriel Gavrilov <79603829+GabrielGavrilov@users.noreply.github.com>
2022-05-07 07:54:26 -0700

    update

---
+++ source-archiver/main.cs
@@ -0,0 +1,23 @@
+﻿class main
+{
+
+    static string webUrl;
+    static string saveLocation;
+
+    public static async Task Main(string[] args)
+    {
+
+        Scraper s = new Scraper();
+        Archiver a = new Archiver();
+
+        Console.Write("Link to source: ");
+        webUrl = Console.ReadLine();
+
+        Console.Write("Save location: ");
+        saveLocation = Console.ReadLine();
+
+        await s.scrapeSource(webUrl);
+        await a.saveSourceToLocation(saveLocation, s.retrieveSource());
+
+    }
+}
